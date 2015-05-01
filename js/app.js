63905431e0d447f48667a083e33adf9108c92524
Sara OC <saramoo@hotmail.com>
2015-05-01 23:29:54 +0100

    first test to display tasks failing

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var toDoAvenger = angular.module('ToDoAvenger', ['ngResource']);
+// this is initialising your angular app
 // an angular module is a collection services, directives, controllers and
 // angular.module is used to configure the $injector
 // not convinced that ngResource is exactly right - will I be interacting
 // with RESTful services?
+// ngResource is defining our dependencies
---
+++ test/toDoController.spec.js
@@ -0,0 +1,22 @@
+describe('toDoController', function() {
+    beforeEach(module('ToDoAvenger'));
+
+    var ctrl;
+
+    beforeEach(inject(function($controller) {
+        ctrl = $controller('toDoController');
+    }));
+
+    var tasks = [
+      {
+        "content": "learn to code Angular apps"
+      },
+      {
+        "content": "get some sleep"
+      }
+    ];
+
+    it('displays tasks', function() {
+        expect(ctrl.content.tasks).toEqual(items);
+    });
+});
