517b41bdd20a51971534fdc04b622f9440f789a5
Sara OC <saramoo@hotmail.com>
2015-05-01 16:34:52 +0100

    set up repo for angular

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var toDoAvenger = angular.module('ToDoAvenger', ['ngResource']);
+// an angular module is a collection services, directives, controllers and
+// angular.module is used to configure the $injector
+// not convinced that ngResource is exactly right - will I be interacting
+// with RESTful services?
